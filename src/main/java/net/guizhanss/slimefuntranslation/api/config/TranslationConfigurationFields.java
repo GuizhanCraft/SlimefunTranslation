9737d8071503f77eb3ae460f9cedbbe8c2f39904
ybw0014 <i@guizhanss.com>
2024-02-13 19:13:28 -0500

    fix: add default back

--- src/main/java/net/guizhanss/slimefuntranslation/api/config/TranslationConfigurationFields.java
+++ src/main/java/net/guizhanss/slimefuntranslation/api/config/TranslationConfigurationFields.java
@@ -3,14 +3,18 @@ package net.guizhanss.slimefuntranslation.api.config;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * This class holds the fields to look for in a translation configuration file.
  */
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public final class TranslationConfigurationFields {
+    public static final TranslationConfigurationFields DEFAULT = TranslationConfigurationFields.builder().build();
+
     @Builder.Default
     private String items = "translations";
     @Builder.Default
